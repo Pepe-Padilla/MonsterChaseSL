6c78f994d23c4cb7fdf0fb1baf2ab3663bec8ed3
José Padilla <jpadilla.isc@gmail.com>
2022-11-09 23:12:15 +0100

    animations, camara jump

---
+++ MonsterChase/Assets/Scripts/CamaraFollow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour
+{
+    // Constants
+    private static string PLAYER_TAG = "Player";
+
+    // SerializedFields
+    [SerializeField]
+    private float minX, maxX;
+
+    // private vars
+    private Transform player;
+    private Vector3 tempPos;
+
+    // Start is called before the first frame update
+    void Start() {
+        player = GameObject.FindWithTag(PLAYER_TAG).transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        tempPos = transform.position;
+        tempPos.x = player.position.x;
+        transform.position = tempPos;
+    }
+}
--- MonsterChase/Assets/Scripts/Player.cs
+++ MonsterChase/Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Constants
-    private static string WALK_ANIMATION = "Walk";
+    private static string WALK_ANIMATION = "walk";
+    private static string GROUND_TAG = "Ground";
 
     // SerializedFields
     [SerializeField]
@@ -18,6 +19,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D myBody;
     private SpriteRenderer sr;
     private Animator anim;
+    private bool isGrounded;
 
     private void Awake() {
         myBody = GetComponent<Rigidbody2D>();
@@ -32,11 +34,41 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update() {
         PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
+
+    private void FixedUpdate() {
+        playerJump();
     }
 
     // Movimiento horizontal (so far)
     private void PlayerMoveKeyboard() {
         movmentX =  Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(movmentX, 0f, 0f) * moveForce * Time.deltaTime;
+    }
+
+    private void AnimatePlayer() {
+        if(movmentX > 0) {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        } else if(movmentX < 0) {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        } else {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+
+    private void playerJump() {
+        if(Input.GetButton("Jump") && isGrounded) {
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f,jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag(GROUND_TAG)) {
+            isGrounded = true;
+        }
     }
 }
